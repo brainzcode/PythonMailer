5ab9375b63847a370afab80524add4fe8d05889e
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-11-17 11:27:29 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # importing the Yagmail library
 import yagmail
 
-my_email = "testpythonmailer1@gmail.com"
-password = "foanjireptgrxvox"
+my_email = "your email@domain.com"
+password = "yourpassword"
 
 
 try:
     # initializing the server connection
     yag = yagmail.SMTP(user=my_email, password=password)
     # sending the email
-    yag.send(to='testpythonmailer2@yahoo.com', subject='Testing Yagmail', contents='<h1>Hurray, Second Email '
+    yag.send(to='receiveremail@domain.com', subject='Testing Yagmail', contents='<h1>Hurray, Second Email '
                                                                                    'worked!</h1> '
              '<p>Just Running multiple tests</p>')
     print("Email sent successfully")
